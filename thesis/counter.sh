12d0cc5583ece90d49b6b240182b193df06a6962
Vendula Poncova <poncovka@gmail.com>
2015-10-15 23:22:26 +0200

    Init

---
+++ thesis/counter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pdftotext -f 6 project.pdf
+chars=$((`cat project.txt | wc -m`))
+echo "Bezny rozsah: 80-100 normostran."
+echo "Aktualni pocet normostran v projektu:" $(($chars / 1800 ))
+echo "Procentualne hotovo:" $(($chars / 1800 * 100 / 80 )) "%"
+echo "Semestralni projekt:" $(($chars / 1800 * 100 / 30 )) "%"
+echo "Počet znaků:" $(($chars))
+rm project.txt
