7e9cae540f722a6b4b9499f309e889b9e55c23a1
Vendula Poncova <poncovka@gmail.com>
2015-10-20 16:35:00 +0200

    Gradle project init

---
+++ app/src/main/java/jbyco/App.java
@@ -0,0 +1,14 @@
+package jbyco;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome!");
+		for(String arg:args) {
+			System.out.println(arg);
+		}
+
+	}
+
+}
