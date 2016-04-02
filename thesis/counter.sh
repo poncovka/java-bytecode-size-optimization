cc8f22f729ba963dfbb51a245b4824b651c1956d
Vendula Poncova <poncovka@gmail.com>
2016-04-02 13:46:49 +0200

    Thesis: merge with semestral

--- thesis/counter.sh
+++ thesis/counter.sh
@@ -2,9 +2,9 @@
 
 pdftotext -f 6 project.pdf
 chars=$((`cat project.txt | wc -m`))
-echo "Bezny rozsah: 80-100 normostran."
-echo "Aktualni pocet normostran v projektu:" $(($chars / 1800 ))
-echo "Procentualne hotovo:" $(($chars / 1800 * 100 / 80 )) "%"
-echo "Semestralni projekt:" $(($chars / 1800 * 100 / 30 )) "%"
-echo "Počet znaků:" $(($chars))
+echo "Počet znaků:         "$(($chars))
+echo "Počet normostran:    "$(($chars / 1800 ))
+echo "Diplomová práce:     "$(($chars / 1800 * 100 / 50 ))"% z minima"
+echo "Minimální rozsah:    ? normostran (? vysázených stran)"
+echo "Bezny rozsah:        ?-? normostran (?-? vysázených stran)"
 rm project.txt
