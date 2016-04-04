b76c4ea2d659818ca69d9ef06510fc18dcae4067
Vendula Poncova <poncovka@gmail.com>
2016-04-04 17:04:36 +0200

    Thesis: used new format

--- thesis/counter.sh
+++ thesis/counter.sh
@@ -5,6 +5,6 @@ chars=$((`cat project.txt | wc -m`))
 echo "Počet znaků:         "$(($chars))
 echo "Počet normostran:    "$(($chars / 1800 ))
 echo "Diplomová práce:     "$(($chars / 1800 * 100 / 50 ))"% z minima"
-echo "Minimální rozsah:    ? normostran (? vysázených stran)"
-echo "Bezny rozsah:        ?-? normostran (?-? vysázených stran)"
+echo "Minimální rozsah:    50 normostran (25 vysázených stran)"
+echo "Bezny rozsah:        80-100 normostran (40-50 vysázených stran)"
 rm project.txt
